8671e3c4de791e8530029de0b3bab0e9edae1775
J0HDev <johnny9511@163.com>
2019-04-02 11:32:32 +0800

    [2019.4.1] 18. 二叉树的层次遍历
    [2019.4.1] 18. 二叉树的层次遍历（看到相关的另一道题目和刚才题目一样，顺便刷掉了）

--- AlgorithmSwift/AlgorithmSwift/19.Binary Tree Level Order Traversal/BinaryTreeLevelOrderTraversal.swift
+++ AlgorithmSwift/AlgorithmSwift/19.Binary Tree Level Order Traversal/BinaryTreeLevelOrderTraversal.swift
@@ -38,7 +38,34 @@ class Queue<Element> : NSObject  {
     }
 }
 
+// 二叉树的层次遍历 II
 func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+    guard let node = root else { return [[Int]]() }
+    var numbers = [[Int]]()
+    let queue = Queue<TreeNode>()
+    queue.enQueue(node)
+    
+    while !queue.isEmptyQueue() {
+        if let elems = queue.deQeueuAll() {
+            var line = [Int]()
+            for elem in elems {
+                line.append(elem.val)
+                if let left = elem.left {
+                    queue.enQueue(left)
+                }
+                if let right = elem.right {
+                    queue.enQueue(right)
+                }
+            }
+            numbers.insert(line, at: 0)
+        }
+    }
+    
+    return numbers
+}
+
+// 二叉树的层次遍历
+func levelOrder(_ root: TreeNode?) -> [[Int]] {
     guard let node = root else { return [[Int]]() }
     var numbers = [[Int]]()
     let queue = Queue<TreeNode>()
